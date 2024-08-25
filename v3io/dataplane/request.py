a3db770922052e2fd0ae3e1b66484c708d76cc92
Gal Topper <gal.topper@gmail.com>
2024-08-25 17:33:56 +0800

    orjson doesn't serialize bytes so don't pass it bytes

--- v3io/dataplane/request.py
+++ v3io/dataplane/request.py
@@ -415,7 +415,7 @@ def _to_base64(input):
     if isinstance(input, str):
         input = input.encode("utf-8")
 
-    return base64.b64encode(input)
+    return base64.b64encode(input).decode("utf-8")
 
 
 def _dict_to_typed_attributes(d):
@@ -448,7 +448,7 @@ def _dict_to_typed_attributes(d):
             type_value = str(value)
         elif attribute_type in [bytes, bytearray]:
             type_key = "B"
-            type_value = base64.b64encode(value)
+            type_value = base64.b64encode(value).decode("utf-8")
         elif isinstance(value, bool):
             type_key = "BOOL"
             type_value = value
